b55ec90852c53487515e29e94bfb61d0fe396848
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-18 17:30:28 +0500

    Parameterization of tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,27 @@
 
+
+import pytest
+
 from src.main import Calculator
 
-def test_divide():
-    x = 1
-    y = 2
-    res = 0.5
+@pytest.mark.parametrize(
+    "x, y, res",
+    [
+        (1, 2, 0.5),
+        (5, -1, -5),
+    ]
+)
+
+def test_divide(x, y, res):
+    assert Calculator() .divide(x, y) == res
+
+
+@pytest.mark.parametrize(
+    "x, y, res",
+    [
+        (1, 2, 3),
+        (5, -1, 4),
+    ]
+)
+def test_add(x, y, res):
     assert Calculator() .divide(x, y) == res
